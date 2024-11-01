e6a7fde3777851bf6fc88a22da344108676b6802
Abhik Patra <theabhik2020@gmail.com>
2024-11-01 12:25:52 +0530

    route issue solved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import {Dashboard} from "@/components/Home/dashboard"
-import AdminPanel from './dashboard/admin/page'
+import HomePage from './(dashboard)/page'
+// import {Dashboard} from "@/components/Home/dashboard"
+// import AdminPanel from './dashboard/admin/page'
 function page() {
   return (
     <div>
-      <AdminPanel/>
+      <HomePage/>
     </div>
   )
 }
