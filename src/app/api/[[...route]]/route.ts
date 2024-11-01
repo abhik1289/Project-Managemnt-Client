dc7be2dbce56727ba15f51cfe84f9c18064ef327
Abhik Patra <theabhik2020@gmail.com>
2024-11-01 12:24:41 +0530

    hono setup

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,15 +1,20 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
 
-export const runtime = 'edge'
+export const runtime = "edge";
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath("/api/v1");
 
-app.get('/hello', (c) => {
+// not found route
+app.notFound((c) => {
   return c.json({
-    message: 'Hello Next.js!',
-  })
-})
+    message: "This route does not exist",
+  });
+});
 
-export const GET = handle(app)
-export const POST = handle(app)
+const auth = new Hono()
+auth.get('/', (c) => c.text('List Books')) 
+app.route('/auth', auth);
+
+export const GET = handle(app);
+export const POST = handle(app);
